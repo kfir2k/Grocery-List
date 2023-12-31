ad708d305b720352e786920a09fd7e1b35c02b4d
kfir <kfir.kotler@gmail.com>
2023-12-31 16:21:41 +0200

    change in js

--- js.js
+++ js.js
@@ -28,8 +28,12 @@ window.addEventListener("load", (listLoded) => {
 			if (item.checkFlag) {
 				let checkBox = document.querySelectorAll(`input[type='checkbox']`)
 				let li = document.querySelectorAll("li")
+				let span = document.querySelectorAll("span")
 				li = li[li.length - 1]
+				span = span[span.length - 1]
 				li.classList.add("overline")
+				span.classList.add("overline")
+
 
 				checkBox = checkBox[checkBox.length - 1]
 				checkBox.checked = true
@@ -87,7 +91,11 @@ function addItemToList(item) {
 	deleteButton.classList.add("garbageCan")
 	const listItem = document.createElement('li');
 
-	const listItemText = `${item.text} : ${item.amounts}`;
+	const listItemText = `${item.text}`;
+	const listItemAmount = `${item.amounts}`
+	const span = document.createElement("span")
+
+
 	deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>';
 	groceryList.appendChild(listItem);
 
@@ -101,11 +109,15 @@ function addItemToList(item) {
 		if (doneCheckBox.checked) {
 			console.log("checked");
 			listItem.classList.add("overline");
+			span.classList.add("overline")
 			item.checkFlag = true
 			saveToLocalStorage();
 			console.log(listArray);
 		} else {
 			listItem.classList.remove("overline");
+			span.classList.remove("overline");
+			item.checkFlag = false
+			saveToLocalStorage();
 
 		}
 
@@ -113,9 +125,10 @@ function addItemToList(item) {
 	});
 
 
-
 	container.appendChild(doneCheckBox)
 	container.append(listItemText)
+	span.append(listItemAmount)
+	container.append(span)
 	listItem.appendChild(container);
 	listItem.appendChild(deleteButton);
 
